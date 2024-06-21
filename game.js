17c72a010de537a1e6853174ee779c3819de5364
Justin <o0justinsgamez0o@gmail.com>
2024-06-21 17:54:17 -0400

    feat: Create game board array  and factory for players

--- game.js
+++ game.js
@@ -4,17 +4,8 @@ const gameboard = (function() {
     // Make an n x n board; default will be n = 3
     var n = 3;
     var boardArr = [];
-    /*
-    I am trying to make a jagged (2D) array where a board is populated like so
-    Assume n=3
-
-    boardArr = [
-        [" "," "," "]
-        [" "," "," "]
-        [" "," "," "]
-    ];
-    */
-
+   
+    // Make jagged array for board
     for (var i = 0; i < n; i++){
         const boardRow = [];
         for (var j = 0; j < n; j++){
@@ -23,10 +14,26 @@ const gameboard = (function() {
         boardArr.push(boardRow);
     }
 
-    temp = JSON.stringify(boardArr);
-    alert(temp);
+    /*
+    Functions
+    */
+    const alertBoard = () => {
+        temp = JSON.stringify(boardArr);
+        alert(temp);
+    }
+    const placePiece = (x, y, piece) => boardArr[x][y] = piece;
+    const getBoard = () => boardArr;
+
+    // Return the functions
+    return {alertBoard, placePiece};
 
 })();
 
+// Factory for players
+function createPlayer(name) {
+    //
+}
+
 // Make instance of gameboard
-// gameboard();
+gameboard.placePiece(0,0, "X");
+gameboard.alertBoard(); // both work
