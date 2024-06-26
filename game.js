c191edd95f2b2c7ec11ee77163877673a93efa12
Justin <o0justinsgamez0o@gmail.com>
2024-06-26 13:50:45 -0400

    chore: Add more functions for checking conditions of game

--- game.js
+++ game.js
@@ -28,13 +28,23 @@ const gameboard = (function() {
                 boardArr[rowIndex][colIndex] = null;})
         })
     }
+    const checkBoard = (currentPlayer, isEmpty) => {
+        // Check if user's attempt is valid
+        if (gameboard.getBoard[input[0]][input[1]] != null){
+            console.log("Invalid; gameboard is not empty here");
+            isEmpty = false;
+            return isEmpty;
+        } else {
+            gameboard.placePiece(input[0], input[1], currentPlayer.piece); 
+        }
+    }
 
     // Return the functions
-    return {alertBoard, placePiece, getBoard, resetBoard};
+    return {alertBoard, placePiece, getBoard, resetBoard, checkBoard};
 
 })();
 
-const startGame = (function() {
+const startGame = (function() { // doesnt need to be Immeadiatley Invoked
 
     // Reset board before each game
     gameboard.resetBoard();
@@ -45,24 +55,51 @@ const startGame = (function() {
     player1 = createPlayer(player1Name, "X");
     player2 = createPlayer(player2Name, "O");
 
-    // Ask player 1 where to put piece
+    gameHandler.getPlayerInput(pass in player);
+
     
-    do {
+
+})();
+
+const gameHandler = (function() {
+
+    // Pass in the current player object and get the piece member
+    const getPlayerInput = (currentPlayer) => {
+        // Ask player 1 where to put piece
         var isEmpty = true;
-        var input = prompt("Where would you like to place the piece? Enter two numbers in this format '0 1'");
-        input.split(" ");
+        do {
+            
+            var input = prompt("Where would you like to place the piece? Enter two numbers in this format '0 1'");
+            input.split(" ");
 
-        // Check if user's attempt is valid
-        if (gameboard.getBoard[input[0]][input[1]] != null){
-            console.log("Invalid; gameboard is not empty here");
-            isEmpty = false;
+            // Call gameboard check func
+            isEmpty = gameboard.checkBoard(currentPlayer, isEmpty);
+
+        } while (isEmpty == true); 
+    } // end playerInput func
+
+    const validateInput = () => {
+        if (playerTurn == true){
+            gameboard.placePiece(x, y, player.piece);
         } else {
-            placePiece(input[0], input[1], createPlayer.piece); // TODO: FIX! should not be createPlayer.piece
-            // I need logic to check this for each player - ideally a function where we pass in the player as a variable
+            console.log("Error; not the current player's turn");
         }
+    }
 
-    } while (isEmpty == true); // fix later
+    const checkForWin = (playedTurns) => {
+        if (playedTurns >= 9) {
+            console.log("Tie! No one wins.");
+            gameboard.resetBoard();
+            // No points awarded for a tie
+        } else {
+            // Check each row and column; diagonal consider later
+            var currentBoard = gameboard.getBoard();
+            
+        }
+    }
 
+    // Return the function
+    return {getPlayerInput, validateInput, checkForWin};
 
 })();
 
